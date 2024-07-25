854235c93c759bd86ffb0385d1cf371eb9f443fe
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-07-26 01:21:42 +0300

    full project

---
+++ src/AddItem.js
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { FaPlus } from "react-icons/fa";
+
+const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+  return (
+    <form className="addForm" onSubmit={handleSubmit}>
+      <label htmlFor="addItem">Add Item</label>
+      <input
+        type="text"
+        autoFocus
+        ref={inputRef}
+        autoComplete="off"
+        id="addItem"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+        placeholder="Add item"
+      />
+      <button
+        type="submit"
+        aria-label="Add Item"
+        onClick={() => inputRef.current.focus()}
+      >
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
+
+export default AddItem;
---
+++ src/Content.js
@@ -0,0 +1,19 @@
+import ItemList from "./ItemList";
+
+const Content = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <div>Your list is empty</div>
+      )}
+    </>
+  );
+};
+
+export default Content;
